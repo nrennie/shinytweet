ac87ed36d26bf40d83231d80d214986f1d5c0d15
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-02-05 23:17:03 +0000

    switch to webR deployment

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM rocker/shiny:4.2.1
-RUN install2.r rsconnect tibble dplyr stringr rtweet htmltools lubridate bslib reactable
-WORKDIR /home/shinytweet
-COPY ui.R ui.R 
-COPY server.R server.R 
-COPY likes.rds likes.rds
-COPY deploy.R deploy.R
-CMD Rscript deploy.R
--- deploy.R
+++ deploy.R
@@ -1,19 +1,3 @@
-library(rsconnect)
+shinylive::export("app", "docs")
+httpuv::runStaticServer("docs/")
 
-# a function to stop the script when one of the variables cannot be found
-# and to strip quotation marks from the secrets when you supplied them
-error_on_missing_name <- function(name) {
-  var <- Sys.getenv(name, unset = NA)
-  if(is.na(var)) {
-    stop(paste0("cannot find ", name, " !"), call. = FALSE)
-  }
-  gsub("\"", "", var)
-}
-
-# Authenticate
-setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
-               token = error_on_missing_name("TOKEN"),
-               secret = error_on_missing_name("SECRET"))
-
-# Deploy the application.
-deployApp(appFiles = c("ui.R", "server.R", "likes.rds"))
